3bc00cc581e958aa85d56ceb713d797f72040376
hengchomroeunnn <hengchomroeun3103@gmail.com>
2023-07-05 17:14:07 +0700

    adjustheader

--- layouts/header.vue
+++ layouts/header.vue
@@ -5,7 +5,7 @@
       <nuxt-link to="/"> <img src="/images/logo.png" /></nuxt-link>
     </div>
     <div
-      class="space-x-[80px] font-bca text-gray-500 text-xl pt-6 max-sm:space-x-[8px] max-sm:text-lg">
+      class="space-x-[80px] font-bca text-gray-500 text-xl pt-6 max-sm:space-x-[8px] max-sm:text-lg max-[400px]:text-[16px] ">
       <nuxt-link
         to="/"
         class="hover:rounded-2xl hover:bg-[#7ED957] hover:text-white pt-2 pb-2 pl-6 pr-6"
@@ -18,7 +18,7 @@
       >
       <nuxt-link
         to="/project"
-        class="max-sm:p-2 border-4 rounded-3xl border-[#7ED957] hover:border-2 hover:rounded-2xl hover:bg-[#7ED957] hover:text-white pt-2 pb-2 pl-6 pr-6"
+        class=" max-sm:border-2 max-sm:p-2 border-4 rounded-3xl border-[#7ED957] hover:border-2 hover:rounded-2xl hover:bg-[#7ED957] hover:text-white pt-2 pb-2 pl-6 pr-6"
         >Project</nuxt-link
       >
     </div>
