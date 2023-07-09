aef1ea95f1c7830df22658cb471297d682e65c3a
hengchomroeunnn <hengchomroeun3103@gmail.com>
2023-07-09 11:51:16 +0700

    socialink

--- layouts/header.vue
+++ layouts/header.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-wrap justify-between border-b-4 pt-2 pb-2">
     <div
       class="flex flex-wrap justify-center items-center w-[80px] max-sm:w-[40px] max-sm:m-4">
-      <nuxt-link to="/"> <img src="/images/logo.png" /></nuxt-link>
+      <nuxt-link :to="localPath('/')"> <img src="/images/logo.png" /></nuxt-link>
     </div>
     <div
       class="space-x-[80px] font-bca text-gray-500 text-xl pt-6 max-sm:space-x-[8px] max-sm:text-lg max-[400px]:text-[16px]">
