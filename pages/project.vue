3b216f8a332776ffcd16928271448f8204fb6e96
hengchomroeunnn <hengchomroeun3103@gmail.com>
2023-07-09 13:04:14 +0700

    project1.0

--- pages/project.vue
+++ pages/project.vue
@@ -1,7 +1,12 @@
 <template>
   <darkMode />
   <div class="text-center pb-12 pt-4 text-2xl aksor max-sm:text-lg">
-    អត់ទាន់មាន <span class=" font-bca">Project</span>​  អីមួយទេ! ជិត​ <span class=" font-bca">Intern</span>​  ហើយចង់តែយំទេ🥹
+    <div class="font-bca">{{ $t("project_title") }}</div>
+  </div>
+  <div class=" text-center pb-12 pt-4 text-2xl aksor max-sm:text-lg">
+    <div class="font-bca">
+      <h1>Project is upcoming...</h1>
+    </div>
   </div>
 </template>
 
@@ -9,7 +14,7 @@
 export default {};
 </script>
 <style>
-.aksor{
-    font-family: 'battambang';
+.aksor {
+  font-family: "battambang";
 }
 </style>
